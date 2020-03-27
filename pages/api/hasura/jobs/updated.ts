88be2977be8d0f35b8cca5cd16aef3548574dbc0
Aron Jones <aron.jones@gmail.com>
2020-03-27 14:01:17 -0400

    add notification loggin

--- pages/api/hasura/jobs/updated.ts
+++ pages/api/hasura/jobs/updated.ts
@@ -63,6 +63,8 @@ export default async function hasurajobUpdatedApi(
     },
   }
 
+  console.log('notification', notification)
+
   console.log(
     await messaging.sendMulticast({
       webpush: { notification },
